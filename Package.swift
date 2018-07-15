641ebb57c5b5435837b48e4de9f2b22d6abbc948
Nikita Zatsepilov <nikita.zatsepilov@rosberry.com>
2018-07-15 17:34:04 +0600

    Remove Linenoise dependency, cleanup console class

--- Package.swift
+++ Package.swift
@@ -8,12 +8,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Zewo/CZeroMQ.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf", from: "1.0.0"),
-        .package(url: "https://github.com/kylef/Commander.git", from: "0.0.0"),
-        .package(url: "https://github.com/andybest/linenoise-swift.git", from: "0.0.0")
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.0.0")
     ],
     targets: [
         .target(name: "Neon", dependencies: [
-            "SwiftProtobuf", "Commander", "LineNoise"
+            "SwiftProtobuf", "Commander"
         ])
     ]
 )
--- Sources/Neon/Console.swift
+++ Sources/Neon/Console.swift
@@ -13,7 +13,6 @@ final class Console {
     private let queue: DispatchQueue
     private var isRunning: Bool = false
     
-    private let lineNoise: LineNoise
     private var consoleGroup: Group!
     
     private let playerService: PlayerServiceProtocol
@@ -24,8 +23,6 @@ final class Console {
         self.objectService = objectService
         
         queue = .init(label: "com.neon.console")
-        lineNoise = LineNoise()
-        lineNoise.setHistoryMaxLength(200)
         consoleGroup = createConsoleGroup()
     }
     
@@ -95,39 +92,6 @@ extension Console {
             print("POSITION: \(player.position)")
             print(String(format: "POINTER: %p", self.playerService.playerPointer))
         }
-        
-        group.command("pointer") {
-            print("POINTER: \(self.playerService.playerPointer)")
-        }
-        
-        group.command("move") { (pointX: Float32, pointY: Float32, pointZ: Float32) in
-            let player = self.objectService.currentPlayer
-            var point = player.position
-            point.y -= 0.001
-            
-            try! ServiceLocator.functionService.clickToMove(action: .move,
-                                                            point: point,
-                                                            targetGuid: player.guid,
-                                                            precision: 0)
-        }
-        
-        group.command("movetest") {
-//            let points = [
-//                Vector3(x: 32.8169, y: 2458.51, z: -4.29713),
-//                Vector3(x: 33.672, y: 2469.14, z: -4.29713),
-//                Vector3(x: 28.2157, y: 2472.06, z: -4.29713),
-//                Vector3(x: 27.1028, y: 2484.7, z: -4.29555),
-//                Vector3(x: 21.4715, y: 2490.49, z: -4.29466),
-//                Vector3(x: 17.6167, y: 2499.29, z: -4.29588),
-//                Vector3(x: 18.3302, y: 2508.11, z: -4.29358)
-//            ]
-//
-//            MovementManager.current.moveByPath(points)
-        }
-        
-        group.command("movecancel") {
-//            MovementManager.current.cancelMove()
-        }
     }
 }
 
@@ -135,8 +99,6 @@ extension Console {
 
 extension Console {
     private func createObjectCommands(in group: Group) {
-        group.command("dump") {
-            
-        }
+    
     }
 }
